0b5308865bab34f6e7d0cc4751f8b68e46e4427b
mauro silva <mauro1400@outlook.com>
2022-11-11 12:40:18 -0400

    funaciona 200%

--- resources/views/SolicitudArticulo/solicitud-articulo/tabla.blade.php
+++ resources/views/SolicitudArticulo/solicitud-articulo/tabla.blade.php
@@ -5,7 +5,7 @@
             <th>Descripcion</th>
             <th>Unidad</th>
             <th>Cantidad</th>
-            <th>Cantidad_Anterior</th>
+            <th>Actualizacion</th>
             <th>Cantidad Entregado</th>
             <th>total Entregado</th>
             <th>Actualizado en</th>
@@ -21,13 +21,12 @@
             <td>{{ $item->description }}</td>
             <td>{{ $item->unit }}</td>
             <td>{{ $item->amount }}</td>
-            <td>{{ $item->Monto_Anterior }}</td>
+            <td>{{ $item->montoP }}</td>
             <td>{{ $item->amount_delivered }}</td>
             <td>{{ $item->total_delivered }}</td>
             <td>{{ $item->updated_at }}</td>
             <td>{{ $item->observacion }}</td>
             <td>
-
                 @if($item->estado == "0")
                 <a href="{{ route('SolicitudArticulo.aprobado',$item->id) }}" class="btn btn-warning btn-sm" role="button" aria-hidden="true">Pendiente</a>
                 @elseif($item->estado == "1")
